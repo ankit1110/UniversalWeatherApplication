34eb1a47d9ab69fcb90dc88b4869ea539053593e
Ankitkumar <ankitrana1110@yahoo.in>
2016-05-31 01:24:36 -0400

    Add GPS Tracker and get current location

--- app/src/main/java/ohmsoftwaresinc/com/universalweatherapplication/MapsActivity.java
+++ app/src/main/java/ohmsoftwaresinc/com/universalweatherapplication/MapsActivity.java
@@ -9,74 +9,49 @@ import android.location.LocationManager;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements LocationListener {
+public class MapsActivity extends FragmentActivity {
 
-    private GoogleMap mMap;
-    private GoogleMap googleMap; // Might be null if Google Play services APK is not available.
-    LocationManager locationManager;
-    String mprovider;
-    double longitude, latitude;
-    Location location;
+    GPSTracker gps;
+    double latitude, longitude;
+    private GoogleMap googleMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-
-
-        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        Criteria criteria = new Criteria();
-
-        mprovider = locationManager.getBestProvider(criteria, false);
-
-        if (mprovider != null && !mprovider.equals("")) {
-
-            location = locationManager.getLastKnownLocation(mprovider);
-            // locationManager.requestLocationUpdates(mprovider,15000, 1,getApplicationContext());
-
-            if (location != null)
-                onLocationChanged(location);
-            //   Toast.makeText(getBaseContext(),+location.getLatitude()+" "+location.getLongitude(), Toast.LENGTH_LONG).show();
-
-
-        }
-
+        get_current_location();
 
         try {
             // Loading map
             initilizeMap();
 
             // Changing map type
-             googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+         //   googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
             // googleMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
-           // googleMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+            googleMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
             // googleMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
             // googleMap.setMapType(GoogleMap.MAP_TYPE_NONE);
 
             // Showing / hiding your current location
-            if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                // TODO: Consider calling
-                //    ActivityCompat#requestPermissions
-                // here to request the missing permissions, and then overriding
-                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                //                                          int[] grantResults)
-                // to handle the case where the user grants the permission. See the documentation
-                // for ActivityCompat#requestPermissions for more details.
-                return;
-            }
             googleMap.setMyLocationEnabled(true);
 
             // Enable / Disable zooming controls
@@ -94,48 +69,68 @@ public class MapsActivity extends FragmentActivity implements LocationListener {
             // Enable / Disable zooming functionality
             googleMap.getUiSettings().setZoomGesturesEnabled(true);
 
-            //	double latitude = 17.385044;
-            //	double longitude = 78.486671;
+            // random latitude and logitude
 
-            latitude = location.getLatitude();
-            longitude = location.getLongitude();
-            MarkerOptions marker = new MarkerOptions().position(
-                    new LatLng(latitude, longitude))
-                    ;
-
-            googleMap.addMarker(new MarkerOptions()
-                    .position(new LatLng(latitude, longitude))
-                    .title("Current Location :- "+latitude+" , "+longitude)).showInfoWindow();
-
-            // googleMap.addMarker(marker);
+            // Move the camera to last position with a zoom level
 
             CameraPosition cameraPosition = new CameraPosition.Builder()
-                    .target(new LatLng(latitude,longitude)).zoom(16).build();
-
+                    .target(new LatLng(latitude,
+                            longitude)).zoom(15).build();
             googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
 
+            Marker marker = googleMap.addMarker(new MarkerOptions()
+                    .position(new LatLng(latitude,longitude))
+                    .title("Your Location :- "+latitude+" , "+longitude));
+            marker.showInfoWindow();
+
+            MarkerOptions marker1 = new MarkerOptions().position(
+                    new LatLng(latitude,longitude))
+                    .title("Hello Maps ");
+            marker1.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_VIOLET));
+           googleMap.addMarker(marker1);
 
 
-        } catch (Exception e) {
+
+
+
+
+
+        } catch (Exception e)
+        {
             e.printStackTrace();
         }
 
 
+    }
+
+    private void get_current_location() {
+        try {
+            gps = new GPSTracker(MapsActivity.this);
+
+            if (gps.canGetLocation()) {
+
+                latitude = gps.getLatitude();
+                longitude = gps.getLongitude();
+
+                // \n is for new line
+                Toast.makeText(getApplicationContext(), "Your Location is - \nLat: " + latitude + "\nLong: " + longitude, Toast.LENGTH_LONG).show();
+            } else {
+                // can't get location
+                // GPS or Network is not enabled
+                // Ask user to enable GPS/network in settings
+                gps.showSettingsAlert();
+            }
+        } catch (Exception e) {
+            Toast.makeText(getApplicationContext(), e.toString(), Toast.LENGTH_LONG).show();
+        }
 
     }
 
-
-    public void next(View v)
-    {
-        Intent i = new Intent(getApplicationContext(),MainActivity.class);
+    public void next(View v) {
+        Intent i = new Intent(getApplicationContext(), MainActivity.class);
         startActivity(i);
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        initilizeMap();
-    }
     private void initilizeMap() {
         if (googleMap == null) {
 
@@ -151,8 +146,6 @@ public class MapsActivity extends FragmentActivity implements LocationListener {
     }
 
 
-    @Override
-    public void onLocationChanged(Location location) {
-
-    }
 }
+
+
