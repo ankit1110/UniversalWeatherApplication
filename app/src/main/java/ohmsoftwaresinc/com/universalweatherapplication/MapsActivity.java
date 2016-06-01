59a8e59c83c9ddb9a0999fbdc43774652d08b0f7
Ankitkumar <ankitrana1110@yahoo.in>
2016-05-31 22:32:28 -0400

    Runtime Permission for API-23

--- app/src/main/java/ohmsoftwaresinc/com/universalweatherapplication/MapsActivity.java
+++ app/src/main/java/ohmsoftwaresinc/com/universalweatherapplication/MapsActivity.java
@@ -10,6 +10,7 @@ import android.location.LocationManager;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
@@ -31,27 +32,51 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity {
 
+    private static final int PERMISSION_REQUEST_CODE_LOCATION = 1;
     GPSTracker gps;
     double latitude, longitude;
     private GoogleMap googleMap;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        get_current_location();
 
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED)
+        {
+            ActivityCompat.requestPermissions(this, new String[] { Manifest.permission.ACCESS_COARSE_LOCATION },
+                PERMISSION_REQUEST_CODE_LOCATION);
+
+        //    get_current_location();
+
+          //  load_map();
+
+
+        }
+        else
+        {
+
+            get_current_location();
+            load_map();
+        }
+
+
+
+    }
+
+    public void load_map() {
         try {
             // Loading map
-            initilizeMap();
-
+            get_current_location();
             // Changing map type
             //   googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
             // googleMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
             googleMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
             // googleMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
             // googleMap.setMapType(GoogleMap.MAP_TYPE_NONE);
-
             // Showing / hiding your current location
             if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
                 // TODO: Consider calling
@@ -61,9 +86,10 @@ public class MapsActivity extends FragmentActivity {
                 //                                          int[] grantResults)
                 // to handle the case where the user grants the permission. See the documentation
                 // for ActivityCompat#requestPermissions for more details.
+
                 return;
             }
-            googleMap.setMyLocationEnabled(true);
+            //    googleMap.setMyLocationEnabled(true);
 
             // Enable / Disable zooming controls
             googleMap.getUiSettings().setZoomControlsEnabled(false);
@@ -73,7 +99,6 @@ public class MapsActivity extends FragmentActivity {
 
             // Enable / Disable Compass icon
             googleMap.getUiSettings().setCompassEnabled(true);
-
             // Enable / Disable Rotate gesture
             googleMap.getUiSettings().setRotateGesturesEnabled(true);
 
@@ -90,30 +115,23 @@ public class MapsActivity extends FragmentActivity {
             googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
 
             Marker marker = googleMap.addMarker(new MarkerOptions()
-                    .position(new LatLng(latitude,longitude))
-                    .title("Your Location :- "+latitude+" , "+longitude));
+                    .position(new LatLng(latitude, longitude))
+                    .title("Your Location :- " + latitude + " , " + longitude));
             marker.showInfoWindow();
 
             MarkerOptions marker1 = new MarkerOptions().position(
-                    new LatLng(latitude,longitude))
+                    new LatLng(latitude, longitude))
                     .title("Hello Maps ");
             marker1.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_VIOLET));
-           googleMap.addMarker(marker1);
+            googleMap.addMarker(marker1);
 
 
-
-
-
-
-
-        } catch (Exception e)
-        {
-            e.printStackTrace();
+        } catch (Exception e) {
+            // e.printStackTrace();
+            Toast.makeText(MapsActivity.this, e.toString(), Toast.LENGTH_LONG).show();
         }
 
-
     }
-
     private void get_current_location() {
         try {
             gps = new GPSTracker(MapsActivity.this);
@@ -122,7 +140,7 @@ public class MapsActivity extends FragmentActivity {
 
                 latitude = gps.getLatitude();
                 longitude = gps.getLongitude();
-
+                initilizeMap();
                 // \n is for new line
                 Toast.makeText(getApplicationContext(), "Your Location is - \nLat: " + latitude + "\nLong: " + longitude, Toast.LENGTH_LONG).show();
             } else {
@@ -143,6 +161,25 @@ public class MapsActivity extends FragmentActivity {
     }
 
 */
+   @Override
+   public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+       switch (requestCode) {
+           case PERMISSION_REQUEST_CODE_LOCATION:
+               if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                   // All good!
+            //       get_current_location();
+                   get_current_location();
+                   load_map();
+
+               } else {
+                   //Toast.makeText(this, "Need your location!", Toast.LENGTH_SHORT).show();
+                 //  get_current_location();
+                  // load_map();
+               }
+
+               break;
+       }
+   }
     private void initilizeMap() {
         if (googleMap == null) {
 
